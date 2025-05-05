3c002e4a63be21d5540f53cde0af13b9472adc1e
Shuchit Singh <shuchit.official@gmail.com>
2025-05-06 00:35:09 +0530

    bug fixed

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] })
 
-export const metadata = {
+const metadata = {
   title: "MudraNest",
   description: "AI Powered Finance Application",
 };
