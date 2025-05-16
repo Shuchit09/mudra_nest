95b36afb51466d088d91a630e920cbb9adbbb8cb
Shuchit Singh <shuchit.official@gmail.com>
2025-05-16 17:32:54 +0530

    minor ui changes

--- components/header.jsx
+++ components/header.jsx
@@ -7,13 +7,13 @@ import {
 } from '@clerk/nextjs'
 import Link from 'next/link'
 import { Button } from './ui/button'
-import { LayoutDashboard } from 'lucide-react'
+import { CreditCard, LayoutDashboard } from 'lucide-react'
 import { checkUser } from '@/lib/checkUser'
 
 const Header = async () => {
 
   await checkUser();
-  
+
   return (
     <div className='fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b'>
       <nav className='container mx-auto px-4 py-4 flex items-center justify-between'>
@@ -23,14 +23,14 @@ const Header = async () => {
         <div className='flex items-center space-x-4'>
           <SignedIn>
             <Link href={"/dashboard"} className='text-gray-600 hover:text-blue-600 flex items-center gap-2'>
-              <Button variant='outline'>
+              <Button  variant='outline'>
                 <LayoutDashboard size={18} />
                 <span className='hidden md:inline'>Dashboard</span>
               </Button>
             </Link>
             <Link href={"/transaction/create"}>
               <Button className='flex items-center gap-2'>
-                <LayoutDashboard size={18} />
+                <CreditCard size={18} />
                 <span className='hidden md:inline'>Add Transaction</span>
               </Button>
             </Link>
